d64f9814879d661e88e138e587040fc12dc728b8
xenoformlabs <xenoformlabs@gmail.com>
2020-05-03 15:17:51 -0700

    stable working version

--- DatapodsAQSensor.ino
+++ DatapodsAQSensor.ino
@@ -5,17 +5,24 @@
   Uses the Plantower AQ sensor
   Routes the PM 2.5 and PM 10 results to the analog output pins for
   the Datapods to use as their self-contained input
- 
+
+  We use a hard delay() in the loop, as the asynchronous timer tends to
+  slow things down. At 500ms, we still get "no data" for every 30% of the calls,
+  so this number is okay.
+
+  We can always see serial ouput without a significant penality to the code performance.
  */
 
 #include <SoftwareSerial.h>
-//SoftwareSerial pmsSerial(34, 35);
-SoftwareSerial pmsSerial(27, 33);
 
+SoftwareSerial pmsSerial(27, 33);
 
 #define pm25Pin (25)
 #define pm100Pin (26)
 
+#define MAX_xxx
+
+// PM values, for output to DAC
 uint16_t  pm100Val;
 uint16_t  pm25Val;
 
@@ -45,37 +52,19 @@ struct pms5003data {
 
 struct pms5003data data;
     
-void loop() {
+void loop() {   
   if (readPMSdata(&pmsSerial)) {
     pm25Val = data.pm25_standard;
     pm100Val = data.pm100_standard;
-    
-    // reading data was successful!
-    Serial.println();
-    Serial.println("---------------------------------------");
-    Serial.println("Concentration Units (standard)");
-    Serial.print("PM 1.0: "); Serial.print(data.pm10_standard);
-    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_standard);
-    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_standard);
-    Serial.println("---------------------------------------");
-    Serial.println("Concentration Units (environmental)");
-    Serial.print("PM 1.0: "); Serial.print(data.pm10_env);
-    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_env);
-    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_env);
-    Serial.println("---------------------------------------");
-    Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
-    Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
-    Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
-    Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
-    Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
-    Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
-    Serial.println("---------------------------------------");
 
+    //serialOutFullRead();
+    serialOutSmallRead();
     outputPMValues();
   }
   else
     Serial.println("no data");
 
+  // 100 ms / read, data reads happen more slowly in most caases
   delay(500);
 }
 
@@ -131,6 +120,50 @@ boolean readPMSdata(Stream *s) {
 
 void outputPMValues() {
   // 8-bit values
-   dacWrite(pm25Pin, pm25Val);
-   dacWrite(pm100Pin, pm100Val);
+
+  float fp25 = (float)pm25Val/2.0;    
+  float fp10 = (float)pm100Val/2.0;   
+
+
+  // DAC maximum is 255 
+  // you won't normally go this high unless directly in a fire or something like that
+  if( fp25 > 255.0 )
+    fp25 = 255.0;
+  
+  if( fp10 > 255.0 )
+    fp10 = 255.0;
+ 
+   dacWrite(pm25Pin, fp25);
+   dacWrite(pm100Pin, fp10);
+}
+
+void serialOutSmallRead() {
+    Serial.println("-----");
+    Serial.print("PM 2.5: ");
+    Serial.print(data.pm25_standard);
+    Serial.print("\t\tPM 10: ");
+    Serial.println(data.pm100_standard);
+} 
+
+void serialOutFullRead() { 
+  // reading data was successful!
+    Serial.println();
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (standard)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_standard);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_standard);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_standard);
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (environmental)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_env);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_env);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_env);
+    Serial.println("---------------------------------------");
+    Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
+    Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
+    Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
+    Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
+    Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
+    Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
+    Serial.println("---------------------------------------");
 }
